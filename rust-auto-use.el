115af8ca7d50fea0b45db0d33db7264d7eaa944c
Rotem Yaari <vmalloc@gmail.com>
2018-11-15 15:53:33 +0200

    Use auto-insert boilerplate

--- rust-auto-use.el
+++ rust-auto-use.el
@@ -1,13 +1,30 @@
-;;; rust-auto-use.el --- Automatically inserts Rust `use` lines
+;;; rust-auto-use.el --- Utility to automatically insert Rust use statements  -*- lexical-binding: t; -*-
 
-;;; Homepage: https://github.com/vmalloc/rust-auto-use.el
+;; Copyright (C) 2018  Rotem Yaari
 
-;;; Version: 0.1.0
+;; Author: Rotem Yaari <rotemy@MBP.local>
+;; Keywords: languages
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;; Given a current symbol, this package allows developers to automatically insert Rust `use` statements for importing the symbol.
+
+;; This library allows you to automatically insert rust `use` statements based
+;; on the current symbol
 
 ;;; Code:
+
 
 (defvar rust-auto-use--symbol-cache)
 
@@ -38,7 +55,7 @@
   (let ((symbol (symbol-at-point)))
     (let ((cached-result (gethash symbol rust-auto-use--symbol-cache)))
       (if cached-result cached-result (rust-auto-use--cache-result symbol (format "use %s::%s;"
-                                                                                (read-string (format "import %s from? " symbol)) symbol))))))
+                                                                                  (read-string (format "import %s from? " symbol)) symbol))))))
 
 
 
